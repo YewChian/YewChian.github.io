934ab9da0dc698c540138d1ed5ea674a9f374075
Yew Chian's Dell <mykatsudon@gmail.com>
2024-10-20 19:45:03 +0800

    minimal version completed with three games

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import FWSIcon from './assets/FWSIcon.png'
 import BabyThrowingSimulatorVideo from './videos/BabyThrowingSimulatorVideo.mp4'
+import BabyThrowingSimulatorPlaythrough from './videos/CatMayorsVideo.mp4'
 import FirestormFarmingVideo from './videos/FirestormFarmingVideo.mp4'
-import CatMayorsVideo from './videos/FirestormFarmingVideo.mp4'
+import FirestormFarmingPlaythrough from './videos/FirestormFarmingVideo.mp4'
+import CatMayorsVideo from './videos/CatMayorsVideo.mp4'
+import CatMayorsPlaythrough from './videos/CatMayorsVideo.mp4'
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
 
 function App() {
-
-  return (
-    <div className="app-container">
-      <div className="header">
-        <a href="https://react.dev">
-          <img className="logo" src={FWSIcon}/>
-        </a>
-      </div>
+	const Games = () => {
+		return (
 			<div className="container">
-
 				<a className="card" href="/src/BabyThrowingSimulatorIsolated/index.html" target="_blank">
 					<h2>Baby Throwing Simulator</h2>
 					<video loop autoPlay muted onClick="/src/BabyThrowingSimulatorIsolated/index.html">
@@ -43,9 +43,59 @@ function App() {
 							type="video/mp4"
 						/>
 					</video>
-					<h3>2-player cat-themed building sim. Tutorial in progress.</h3>
+					<h3>2-player cat-themed building sim</h3>
 				</a>
 			</div>
+		)
+	}
+
+	const GameplayVideos = () => {
+		return (
+			<div className="container">
+				<h2>Baby Throwing Simulator</h2>
+				<video controls>
+					<source
+						src={BabyThrowingSimulatorPlaythrough}
+						type="video/mp4"
+					/>
+				</video>
+				<h2>Firestorm Farming</h2>
+				<video controls>
+					<source
+						src={FirestormFarmingPlaythrough}
+						type="video/mp4"
+					/>
+				</video>
+				<h2>Cat Mayors</h2>
+				<video controls>
+					<source
+						src={CatMayorsPlaythrough}
+						type="video/mp4"
+					/>
+				</video>
+			</div>
+		)
+	}
+
+  return (
+    <div className="app-container">
+      <div className="header">
+        <a href="/">
+          <img className="logo" src={FWSIcon}/>
+        </a>
+				<Router>
+					<Link to="/">
+						Home
+					</Link>
+					<Link to="/gameplay">
+						Gameplay Videos
+					</Link>
+					<Routes>
+						<Route path='/' element={<Games />} />
+						<Route path='/gameplay' element={<GameplayVideos />} />
+					</Routes>
+				</Router>
+      </div>
     </div>
   )
 }
--- src/CatMayors/index.service.worker.js
+++ src/CatMayors/index.service.worker.js
@@ -4,9 +4,9 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1729274303|70392548';
+const CACHE_VERSION = '1729406295|18944453123';
 /** @type {string} */
-const CACHE_PREFIX = 'BabyTossSimulato-sw-cache-';
+const CACHE_PREFIX = 'CatBirdCity-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
