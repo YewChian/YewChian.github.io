b374636131b081c0a9a61d92f63a84a0d90c830a
Yew Chian's Dell <mykatsudon@gmail.com>
2024-10-21 15:50:10 +0800

    added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import FirestormFarmingVideo from './videos/FirestormFarmingVideo.mp4'
 import FirestormFarmingPlaythrough from './videos/FirestormFarmingVideo.mp4'
 import CatMayorsVideo from './videos/CatMayorsVideo.mp4'
 import CatMayorsPlaythrough from './videos/CatMayorsVideo.mp4'
+//import BabyThrowingSimulator from './BabyThrowingSimIsolated/index.html'
 import './App.css'
 import {
-  BrowserRouter as Router,
-  Routes, Route, Link
+	createBrowserRouter,
+	RouterProvider,
 } from 'react-router-dom'
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
 			<div className="container">
 				<a className="card" href="/src/BabyThrowingSimulatorIsolated/index.html" target="_blank">
 					<h2>Baby Throwing Simulator</h2>
-					<video loop autoPlay muted onClick="/src/BabyThrowingSimulatorIsolated/index.html">
+					<video loop autoPlay muted onClick="/src/videos/BabyThrowingSimulatorVideo">
 						<source 
 							src={BabyThrowingSimulatorVideo}
 							type="video/mp4"
@@ -77,25 +78,39 @@ function App() {
 		)
 	}
 
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: <Games />,
+		},
+		{
+			path: '/gameplay',
+			element: <GameplayVideos /> ,
+		},
+		/*
+		{
+			path: '/BabyThrowingSimulatorIsolated',
+			element: <BabyThrowingSimulator />,
+		}
+		*/
+	]);
+
+
+
   return (
     <div className="app-container">
       <div className="header">
         <a href="/">
           <img className="logo" src={FWSIcon}/>
         </a>
-				<Router>
-					<Link to="/">
-						Home
-					</Link>
-					<Link to="/gameplay">
-						Gameplay Videos
-					</Link>
-					<Routes>
-						<Route path='/' element={<Games />} />
-						<Route path='/gameplay' element={<GameplayVideos />} />
-					</Routes>
-				</Router>
+        <a href="/">
+					Home
+        </a>
+        <a href="/gameplay">
+					Playthroughs	
+        </a>
       </div>
+			<RouterProvider router={router} />
     </div>
   )
 }
